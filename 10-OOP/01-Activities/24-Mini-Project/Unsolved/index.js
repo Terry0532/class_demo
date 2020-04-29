09281482f05858a0ee4b9f92db426a267a3394db
Terry <t3rrygong@gmail.com>
2020-04-29 10:50:36 -0600

    update

--- 10-OOP/01-Activities/24-Mini-Project/Unsolved/index.js
+++ 10-OOP/01-Activities/24-Mini-Project/Unsolved/index.js
@@ -1,18 +1,8 @@
 const inquirer = require('inquirer');
 const axios = require('axios');
 const Word = require('./word');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-function askUserEnterLetter() {
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "ENTER ONE LETTER",
-            name: "guess"
-        }
-    ]).then(function (res) {
-        return res.guess;
-    });
-};
 // function askUserEnterLetter() {
 //     return new Promise(resolve => {
 //         inquirer.prompt([
@@ -26,28 +16,6 @@ function askUserEnterLetter() {
 //         });
 //     });
 // };
-
-function getUser() {
-    try {
-        const response = axios.get("http://random-word-api.herokuapp.com/word?number=1");
-        console.log(response.data);
-
-        const word1 = new Word(response.data.toString());
-
-        word1.placeholder();
-
-        let counter = 0;
-        while (counter < 3) {
-            let guess = askUserEnterLetter();
-            console.log(guess);
-            word1.guessLetter(guess);
-            counter++;
-        }
-
-    } catch (error) {
-        console.error(error);
-    }
-}
 // async function getUser() {
 //     try {
 //         const response = await axios.get("http://random-word-api.herokuapp.com/word?number=1");
@@ -72,13 +40,10 @@ function getUser() {
 
 // getUser();
 
-function init() {
-    let counter = 0;
-    while (counter < 2) {
-        console.log("123");
-        getUser();
-        counter++;
-    }
-}
+var request = new XMLHttpRequest();
+request.open('GET', "http://random-word-api.herokuapp.com/word?number=1", false);  // `false` makes the request synchronous
+request.send(null);
 
-init();
+if (request.status === 200) {
+    console.log(JSON.parse(request.responseText).toString());
+}
